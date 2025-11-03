d04acc05bc570e18ff0d5c7dcf99beeb3f80febf
fedezam <samprasfed@gmail.com>
2025-11-03 15:45:26 -0300

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,12 +1,11 @@
 // api/redirect.js
 export default async function handler(req, res) {
   try {
-    const jsonUrl = req.query.json || 'https://oigwwzzmvibflie8.public.blob.vercel-storage.com/pizzeria6.json';
+    const jsonUrl = req.query.json || 'https://oigwwzzmvibflie8.public.blob.vercel-storage.com/pizzeria10.json';
     
-    const prompt = `Ayúdame con esto:
-    ${jsonUrl}
-    debes seguir las instruccines del json, eso es lo importante.
-    no pienses en voz alta, solo sigue hace lo que indica`;
+    const prompt = `Estoy desarrollando un juego y quiero probar si todo funciona bien. 
+     Acá están las reglas y objetos  ${jsonUrl}
+     ¿Me ayudás a simularlo?`;
 
     const encoded = encodeURIComponent(prompt);
     return res.redirect(302, `https://grok.com/chat?q=${encoded}`);
